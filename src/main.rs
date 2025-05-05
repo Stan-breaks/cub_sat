0776ab82f13cb8dd4868cf02bb0a807ff6a8d1b5
Stan-breaks <stanleymwendwa03@gmail.com>
2025-05-05 19:52:56 +0300

    reference counting

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,23 @@
-#[allow(unused_variables, dead_code)]
-#[derive(Debug)]
-struct CubeSat {
-    id: u64,
-    mailbox: Mailbox,
-}
-impl CubeSat {
-    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
-        mailbox.deliver(&self)
-    }
-}
-#[derive(Debug)]
-enum StatusMessage {
-    Ok,
-}
+use std::{cell::RefCell, rc::Rc};
 
 #[derive(Debug)]
-struct Mailbox {
-    messages: Vec<Message>,
+struct Groundstation {
+    radio_freq: f64,
 }
-impl Mailbox {
-    fn post(&mut self, msg: Message) {
-        self.messages.push(msg);
-    }
-    fn deliver(&mut self, receipent: &CubeSat) -> Option<Message> {
-        for i in 0..self.messages.len() {
-            if self.messages[i].to == receipent.id {
-                let msg = self.messages.remove(i);
-                return Some(msg);
-            }
-        }
-        None
-    }
-}
-
-#[derive(Debug)]
-struct Message {
-    to: u64,
-    content: String,
-}
-
-struct Groundstation {}
-
-impl Groundstation {
-    fn connect(&self, sat_id: u64) -> CubeSat {
-        CubeSat {
-            id: sat_id,
-            mailbox: Mailbox { messages: vec![] },
-        }
-    }
-    fn send(&self, mailbox: &mut Mailbox, msg: Message) {
-        mailbox.post(msg);
-    }
-}
-
-fn check_status(sat_id: &CubeSat) {
-    println!("{:?}: {:?}", sat_id.id, StatusMessage::Ok);
-}
-
-fn fetch_sat_ids() -> Vec<u64> {
-    vec![1, 2, 3]
-}
-
 fn main() {
-    let mut mail = Mailbox { messages: vec![] };
-    let base = Groundstation {};
-    let sat_ids = fetch_sat_ids();
-    for sat_id in sat_ids {
-        let msg = Message {
-            to: sat_id,
-            content: String::from("hello"),
-        };
-        base.send(&mut mail, msg)
+    let base: Rc<RefCell<Groundstation>> =
+        Rc::new(RefCell::new(Groundstation { radio_freq: 87.65 }));
+    println!("base: {:?}", base);
+    {
+        let mut base_2 = base.borrow_mut();
+        base_2.radio_freq -= 12.34;
+        println!("base_2: {:?}", base_2);
     }
-    let sat_ids = fetch_sat_ids();
-    for sat_id in sat_ids {
-        let sat = base.connect(sat_id);
-        let msg = sat.recv(&mut mail);
-        println!("{:?}: {:?}", sat, msg);
-    }
+    println!("base: {:?}", base);
+
+    let mut base_3 = base.borrow_mut();
+    base_3.radio_freq += 43.21;
+
+    println!("base: {:?}", base);
+    println!("base_3: {:?}", base_3)
 }
